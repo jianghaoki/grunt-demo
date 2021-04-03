dc458a4c8ce80dc2c9b9b97e4757670fc980aa08
jianghao <563110789@qq.com>
2021-04-03 10:46:25 +0800

    test

---
+++ calc.js
@@ -0,0 +1,24 @@
+function add(x, y) {
+  var _x = Number(x),
+      _y = Number(y);
+
+  if(isNaN(_x) || isNaN(_y)) {
+      return NaN;
+    } else {
+        return _x + _y;
+      }
+}
+
+module.exports = add;function add(x, y) {
+  var _x = Number(x),
+      _y = Number(y);
+
+  if(isNaN(_x) || isNaN(_y)) {
+      return NaN;
+    } else {
+        return _x + _y;
+      }
+}
+
+module.exports = add;
+
